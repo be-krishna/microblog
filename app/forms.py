78f1bff5bde7a0d81f5c382a972b2fc8cfae1708
be-krishna <bkshgnj@gmail.com>
2020-08-15 10:19:49 +0530

    added edit_profile page

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
@@ -28,5 +28,9 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use different email address.')
         
 
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
 
 
